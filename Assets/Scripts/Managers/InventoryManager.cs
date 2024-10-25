f5f93fa3a5ac3688952f25307b5609e9dcffe90f
MYAUNTIESPASSIVEAGGRESSIVE <adamkw123lightning@gmail.com>
2024-10-25 21:43:24 +0100

    hahaha

---
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<string> InvItems = new List<string>();
+    public List<string> InvCode = new List<string>();
+
+    //public Dictionary<GameObject> InvObjects = new Dictionary<GameObject>();
+}
